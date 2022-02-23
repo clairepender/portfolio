b3ec22882bec6dfd4a8cf5c8a3b45fdca13ad708
clairepender <clairepender5@gmail.com>
2022-02-23 11:57:42 -0600

    more styling on projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,11 +9,14 @@ const Projects = () => {
        
                 <div className="project-container">
                     <h5 className="text-center p-3 projects-title">My Projects:</h5>
-            
-                    <p className="project-image"><img src={require('../images/FitnessTracker_Screenshot.png')} alt="..." 
-                    style={{width: 800, height: 500}}
-                    /></p>
-
+                        <div className="container-body p-3">
+                            <h2>Fitness Tracker</h2>
+                            <h5>info here</h5>
+                            <h5>link here</h5>
+                            <p className="project-image"><img src={require('../images/FitnessTracker_Screenshot.png')} alt="..." 
+                            style={{width: 600, height: 400}}
+                            /></p>
+                        </div>
                 </div> 
         </div>
     )
