1de37dc8d7c866e25ff2eabc4ae8032733d06629
clairepender <clairepender5@gmail.com>
2022-03-07 11:09:32 -0600

    finishing touches... for now

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const App = () => {
         <NavBar />
                
            
-        <Route exact path="/" render={(routeProps) => <Home {...routeProps} />} />
+        <Route path="/" render={(routeProps) => <Home {...routeProps} />} />
         <Route path="/home" render={(routeProps) => <Home {...routeProps} />} />
         <Route path="/tech" render={(routeProps) => <Tech {...routeProps} />} />
         <Route path="/projects" render={(routeProps) => <Projects {...routeProps} />} />
